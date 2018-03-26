e4caa6c2e61d01a35f403f91c5d2b5c5facda04a
JOE-PC\Joe <jkperin@stanford.edu>
2018-03-26 14:35:58 -0700

    Classes ADC and DAC replace functions adc.m and dac.m

--- components/ADC.m
+++ components/ADC.m
@@ -1,106 +1,108 @@
-function [ys, varQ, xa] = adc(x, ADC, sim, Hrx)
-%% Analog-to-digital conversion: filter, downsample, and quantize signal x
-% Inputs:
-% - x : input signal
-% - ADC : struct with ADC parameters. 
-%    - ros : oversampling ratio of ADC with respect to symbol rate
-%    - ENOB : effective number of bits. Quantization is only
-%    applied if sim.quantiz = true and ADC.ENOB not Inf
-%    - filt : ADC antialiasing filter. Designed with design_filter.m. If filt
-%    is empty then, filtering is not performed.
-%    - offset (optional, default = 0) : additional time offset (in number 
-%    of samples) to be removed i.e., x(t) = x(t - offset/fs). 
-%       * Offset is only removed if filt is not empty.
-%       * Offset need not be integer.
-%    - rclip (optional, default = 0): clipping ratio. Clipping ratio is 
-%    defined as the percentage of the signal amplitude that is clipped in 
-%    both extremes (see code).
-%    - excursion (optional, default = []): excursion limits of DAC. Specify
-%    as a [xmin, xmax]. 
-% - sim : simulation parameters
-%     - f : frequency vector
-%     - fs : sampling frequency to emulate continuous time
-%     - Mct : oversampling ratio to emulate continuous time
-% - Hrx (optional) : if Hrx is provided ADC.filt is ignored and Hrx is
-% applied isntead. Typically, Hrx is used when we want to do matched
-% filtering rather than antialiasing filter.
-% Outputs:
-% - y : output signal
-% - varQ : quantization noise variance
-% - xa : signal after receiver filtering (before sampling)
-
-% Filter
-% Filter by lowpass filter (antialiasing filter)
-if not(exist('Hrx', 'var')) % check if ADC filter is defined
-    Hrx = ifftshift(ADC.filt.H(sim.f/sim.fs)); % rx filter frequency response
-else % if Hrx exists use Hrx instead. This is typically a matched filter
-    Hrx = ifftshift(Hrx); % time shift so that first sample correspond to the center of symbol
-end
-
-if isfield(ADC, 'offset') && ADC.offset ~= 0 % time shift in number of samples. Need not be integer
-    Hshift = ifftshift(exp(1j*2*pi*sim.f/sim.fs*ADC.offset)); % time shift to make first sample center of first pulse
-else
-    Hshift = 1;
-end
-
-% Filter
-xa = real(ifft(fft(x).*Hrx.*Hshift));
-
-% Align to time reference
-if isfield(ADC, 'timeRefSignal')
-    [c, lags] = xcorr(ADC.timeRefSignal, xa);
-    [~, idx] = max(abs(c));
-    if lags(idx) ~= 0
-        xa = circshift(xa, [0 lags(idx)]);
-        fprintf('ADC: input signal was delayed by %d samples (%.2f ps) to match time reference signal\n', lags(idx), 1e12*lags(idx)/sim.fs);
-    end
-end
-
-% Downsample
-if isInteger(sim.Mct/ADC.ros) % resampling is not required
-    xs = xa(1:sim.Mct/ADC.ros:end);
-else
-    [N, D] = rat(ADC.ros/sim.Mct);
-    fprintf('ADC: sim.Mct/ADC.ros is not interger, so signal was resampled by %d/%d.\n', N, D);
-    xs = resample(xa, N, D);
-end
-
-% Quantize
-if isfield(sim, 'quantiz') && sim.quantiz && ~isinf(ADC.ENOB)
-    enob = ADC.ENOB;
-    if isfield(ADC, 'rclip')
-        rclip = ADC.rclip;
-    else
-        rclip = 0;
+classdef ADC
+    %% Analog-to-digital converter
+    properties
+        T % sampling period (in samples)
+        ENOB = Inf; % effective number of bits
+        Filter % instance of class Filter used as anti-aliasing filter
+        offset = 0 % additional time offset (in number of samples) to be removed i.e., x(t) = x(t - offset/fs). Need not be integer
+        excursion = [] % excursion limits. Specify as a [xmin, xmax]
+        rclip = 0 % clipping ratio. Clipping ratio is defined as the percentage of the signal amplitude that is clipped in both extremes (see code)
     end
     
-    if isfield(ADC, 'excursion') && not(isempty(ADC.excursion))
-        xmax = ADC.excursion(2);
-        xmin = ADC.excursion(1);
-    else
-        xmax = max(xs);
-        xmin = min(xs);
+    methods
+        function obj = ADC(T, ENOB, Filter)
+            %% Constructor
+            obj.T = T;
+            obj.ENOB = ENOB;
+            obj.Filter = Filter;
+        end
+        
+        function [xq, xs, xa] = convert(self, x, f, timeRefSignal)
+            %% Main method: Anti-aliasing filter -> sampling -> quantize
+            xa = self.aa_filter(x, f);
+            if exist('timeRefSignal', 'var') % check whether time reference was passed
+                xs = self.sample(xa, timeRefSignal);
+            else
+                xs = self.sample(xa);
+            end
+            xq = self.quantize(xs);
+        end
+        
+        function xa = aa_filter(self, x, f)
+            %% Filter by antialiasing filter defined in class properties (Filter)
+            % f is normalized frequency from -0.5 to 0.5            
+            if self.offset ~= 0 % time shift in number of samples. Need not be integer
+                Hshift = ifftshift(exp(1j*2*pi*f*self.offset));
+            else
+                Hshift = 1;
+            end
+            
+            Haa = ifftshift(self.Filter.H(f));
+            xa = real(ifft(fft(x).*Haa.*Hshift));           
+        end
+        
+        function xs = sample(self, xa, timeRefSignal)
+            %% Sample signal xa with period T (in samples). 
+            % Inputs:
+            % - xa: signal to be sampled (downsampled)
+            % - T: sampling period in samples. T does not need to be an integer
+            % - timeRefSignal (optional): reference signal. xa will be
+            % aligned to timeRefSignal before downsampling
+            
+            % Align to time reference
+            if exist('timeRefSignal', 'var')
+                [c, lags] = xcorr(timeRefSignal, xa);
+                [~, idx] = max(abs(c));
+                if lags(idx) ~= 0
+                    xa = circshift(xa, [0 lags(idx)]);
+                    fprintf('ADC: input signal was delayed by %d samples to match time reference signal\n', lags(idx));
+                end
+            end
+
+            % Downsample
+            if isInteger(self.T) % resampling is not required
+                xs = xa(1:self.T:end);
+            else
+                [N, D] = rat(self.T);
+                fprintf('ADC: T is not interger, so signal was resampled by %d/%d.\n', N, D);
+                xs = resample(xa, N, D);
+            end
+            
+        end
+        
+        function [xq, varQ] = quantize(self, xs)
+            %% Quantization and clipping
+            if not(isinf(self.ENOB))
+                if not(isempty(self.excursion))
+                    xmax = ADC.excursion(2);
+                    xmin = ADC.excursion(1);
+                else
+                    xmax = max(xs);
+                    xmin = min(xs);
+                end
+                xamp = xmax - xmin;    
+
+                % Clipping
+                % clipped: [xmin, xmin + xamp*rclip) and (xmax - xamp*rclip, xmax]
+                % not clipped: [xmin + xamp*rclip, xmax - xamp*rclip]
+                xmin = xmin + xamp*self.rclip; % discounts portion to be clipped
+                xmax = xmax - xamp*self.rclip;
+                xamp = xmax - xmin;
+
+                dx = xamp/(2^(self.ENOB)-1);
+                Pc = mean(xs > xmax | xs < xmin);
+                if Pc ~= 0
+                    fprintf('ADC: clipping probability = %G\n', Pc) 
+                end
+
+                codebook = xmin:dx:xmax;
+                partition = codebook(1:end-1) + dx/2;
+                [~, xq, varQ] = quantiz(xs, partition, codebook); 
+                fprintf('ADC: quantization noise variance = %G | Signal-to-quantization noise ratio = %.2f dB\n', varQ, 10*log10(var(xs)/varQ));
+            else
+                xq = xs;
+                varQ = 0;
+            end
+        end
     end
-    xamp = xmax - xmin;    
-    
-    % Clipping
-    % clipped: [xmin, xmin + xamp*rclip) and (xmax - xamp*rclip, xmax]
-    % not clipped: [xmin + xamp*rclip, xmax - xamp*rclip]
-    xmin = xmin + xamp*rclip; % discounts portion to be clipped
-    xmax = xmax - xamp*rclip;
-    xamp = xmax - xmin;
-    
-    dx = xamp/(2^(enob)-1);
-    Pc = mean(xs > xmax | xs < xmin);
-    if Pc ~= 0
-        fprintf('ADC: clipping probability = %G\n', Pc) 
-    end
-    
-    codebook = xmin:dx:xmax;
-    partition = codebook(1:end-1) + dx/2;
-    [~, ys, varQ] = quantiz(xs, partition, codebook); 
-    fprintf('ADC: quantization noise variance = %G | Signal-to-quantization noise ratio = %.2f dB\n', varQ, 10*log10(var(xs)/varQ));
-else
-    ys = xs;
-    varQ = 0;
 end
--- components/DAC.m
+++ components/DAC.m
@@ -1,117 +1,81 @@
-function [xa, xqzoh, xq] = dac(x, DAC, sim, verbose)
-%% Digital-to-analog conversion: quantize, sample and hold with ZOH, and analog filtering
-% Inputs:
-% - x : input signal already at the DAC sampling rate
-% - DAC : struct with DAC parameters. 
-%    - ros : oversampling ratio of DAC with respect to symbol rate
-%    - resolution : effective DAC resolution in bits. Quantization is only
-%    applied if sim.quantiz = true and DAC.resolution not Inf
-%    - filt : DAC filter after ZOH. Designed with design_filter.m. If filt
-%    is empty then, filtering is not performed.
-%    - offset (optional, default = 0) : additional time offset (in number 
-%    of samples) to be removed i.e., xa(t) = xa(t - offset/fs). 
-%       * Offset is only removed if filt is not empty.
-%       * Offset need not be integer.
-%    - rclip (optional, default = 0): clipping ratio. Clipping ratio is 
-%    defined as the percentage of the signal amplitude that is clipped in 
-%    both extremes (see code).
-%    - excursion (optional, default = []): excursion limits of DAC. Specify
-%    as a [xmin, xmax]. 
-% - sim : simulation parameters
-%     - f : frequency vector
-%     - fs : sampling frequency to emulate continuous time
-%     - Mct : oversampling ratio to emulate continuous time
-% - verbose (optional, default = false) : whether to plot eye diagram
-% Outputs:
-% - xa : output signal at rate sim.fs
-% - xqzoh : signal before analog filtering (after ZOH)
+classdef DAC
+    %% Digital-to-analog conversion
+    properties
+        Nhold % upsampling factor (integer)
+        resolution % resolution in bits
+        Filter % instance of class filter corresponding to DAC's analog frequency response
+        offset = 0 % additional time offset (in number of samples) to be removed. Need not be integer
+        excursion = [] % excursion limits. Specify as a [xmin, xmax]
+        rclip = 0 % clipping ratio. Clipping ratio is defined as the percentage of the signal amplitude that is clipped in both extremes (see code)
+    end
+    
+    methods
+        function obj = DAC(Nhold, resolution, Filter)
+            obj.Nhold = Nhold;
+            obj.resolution = resolution;
+            obj.Filter = Filter;
+        end
+        
+        function xa = convert(self, x, f)
+            %% Main method
+            xq = self.quantize(x);
+            xa = self.sample_and_hold(xq, f);
+        end
+        
+        function [xq, varQ] = quantize(self, x)
+            %% Quantization and clipping
+            if not(isinf(self.resolution))
+                if not(isempty(self.excursion))
+                    xmax = DAC.excursion(2);
+                    xmin = DAC.excursion(1);
+                else
+                    xmax = max(x);
+                    xmin = min(x);
+                end
+                xamp = xmax - xmin;    
 
-% Quantize
-if isfield(sim, 'quantiz') && sim.quantiz && ~isinf(DAC.resolution)
-    enob = DAC.resolution;
-    if isfield(DAC, 'rclip')
-        rclip = DAC.rclip;
-    else
-        rclip = 0;
+                % Clipping
+                % clipped: [xmin, xmin + xamp*rclip) and (xmax - xamp*rclip, xmax]
+                % not clipped: [xmin + xamp*rclip, xmax - xamp*rclip]
+                xmin = xmin + xamp*self.rclip; % discounts portion to be clipped
+                xmax = xmax - xamp*self.rclip;
+                xamp = xmax - xmin;
+
+                dx = xamp/(2^(self.resolution)-1);
+                Pc = mean(x > xmax | x < xmin);
+                if Pc ~= 0
+                    fprintf('DAC: clipping probability = %G\n', Pc) 
+                end
+
+                codebook = xmin:dx:xmax;
+                partition = codebook(1:end-1) + dx/2;
+                [~, xq, varQ] = quantiz(x, partition, codebook); 
+                fprintf('DAC: quantization noise variance = %G | Signal-to-quantization noise ratio = %.2f dB\n', varQ, 10*log10(var(x)/varQ));
+            else
+                xq = x;
+                varQ = 0;
+            end
+        end
+        
+        function xa = sample_and_hold(self, xq, f)
+            %% Zero-order holder (ZOH)
+            assert(isInteger(self.Nhold), 'DAC: oversampling ratio of DAC must be an integer.');
+            xqzoh = upsample(xq, self.Nhold);
+            xqzoh = filter(ones(1, self.Nhold), 1, xqzoh); % group delay due to ZOH is removed inn the frequency domain
+
+            % Filter
+            Hdac = ifftshift(self.Filter.H(f)); % filter frequency response
+
+            % Time shift. Removes any group delay specified by offset. This is
+            % equivalent to resampling the signal
+            nd = self.offset + (self.Nhold-1)/2; % if offtset==0, only removed offset due to ZOH
+
+            Hshift = ifftshift(exp(1j*2*pi*f*nd));
+
+            % Filtering
+            xa = real(ifft(fft(xqzoh).*Hdac.*Hshift)); % filter
+        end        
     end
-    
-    if isfield(DAC, 'excursion') && not(isempty(DAC.excursion))
-        xmax = DAC.excursion(2);
-        xmin = DAC.excursion(1);
-    else
-        xmax = max(x);
-        xmin = min(x);
-    end
-    xamp = xmax - xmin;    
-    
-    % Clipping
-    % clipped: [xmin, xmin + xamp*rclip) and (xmax - xamp*rclip, xmax]
-    % not clipped: [xmin + xamp*rclip, xmax - xamp*rclip]
-    xmin = xmin + xamp*rclip; % discounts portion to be clipped
-    xmax = xmax - xamp*rclip;
-    xamp = xmax - xmin;
-    
-    dx = xamp/(2^(enob)-1);
-    Pc = mean(x > xmax | x < xmin);
-    if Pc ~= 0
-        fprintf('DAC: clipping probability = %G\n', Pc) 
-    end
-    
-    codebook = xmin:dx:xmax;
-    partition = codebook(1:end-1) + dx/2;
-    [~, xq, varQ] = quantiz(x, partition, codebook); 
-    fprintf('DAC: quantization noise variance = %G | Signal-to-quantization noise ratio = %.2f dB\n', varQ, 10*log10(var(x)/varQ));
-else
-    xq = x;
-    varQ = 0;
 end
-
-% Zero-order holder (ZOH)
-Nhold = sim.Mct/DAC.ros; % number of samples to hold
-assert(floor(Nhold) == ceil(Nhold), 'dac: oversampling ratio of DAC (DAC.ros) must be an integer multiple of oversampling ratio of continuous time (sim.Mct)');
-xqzoh = upsample(xq, Nhold);
-xqzoh = filter(ones(1, Nhold), 1, xqzoh); % group delay due to ZOH is removed inn the frequency domain
-
-% Filtering
-if not(isempty(DAC.filt))
-    % Filter
-    Hdac = ifftshift(DAC.filt.H(sim.f/sim.fs)); % rx filter frequency response
     
-    % Time shift. Removes any group delay specified by offset. This is
-    % equivalent to resampling the signal
-    if isfield(DAC, 'offset')
-        offset = DAC.offset + (Nhold-1)/2; % time offset in number of samples. Need not be integer
-    else
-        offset = (Nhold-1)/2; % if offtset was not defined only removed offset due to ZOH
-    end
-    
-    Hshift = ifftshift(exp(1j*2*pi*sim.f/sim.fs*offset));
-
-    % Filtering
-    xa = real(ifft(fft(xqzoh).*Hdac.*Hshift)); % filter
-else
-    xa = xqzoh;
-end
-                                
-% Plot
-if exist('verbose', 'var') && verbose
-    Ntraces = 100;
-    Nstart = sim.Ndiscard*sim.Mct+1;
-    Nend = min(Nstart + Ntraces*2*sim.Mct, length(xa));
-    figure(301)
-    subplot(221), box on
-    eyediagram(xqzoh(Nstart:Nend), 2*sim.Mct)
-    title('Eye diagram after ZOH')
-    subplot(222), box on
-    eyediagram(xa(Nstart:Nend), 2*sim.Mct)
-%     eyediagram(xa(Nstart:Nend), 2*sim.Mct, sim.Mct, ceil(sim.Mct/2))
-    title('DAC output eye diagram')
-    subplot(223), box on
-    plot(sim.f/1e9, abs(fftshift(fft(xa-mean(xa)))).^2)
-    xlabel('Frequency (GHz')
-    ylabel('|X(f)|^2')
-    title('DAC output spectrum')
-    a = axis;
-    axis([-DAC.fs/1e9 DAC.fs/1e9 a(3:4)])
-    drawnow
-end
+            
